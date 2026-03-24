78536963c09dbfe58b464011db2adace8faa4e19
gaurav-garg007 <ggarg6406@gmail.com>
2026-03-24 23:34:34 +0530

    conf done

--- HTML/script.js
+++ HTML/script.js
@@ -1,9 +1,28 @@
+let body = document.body;
+
+// USER BUTTON
 let profile = document.querySelector('.header .flex .profile');
+document.querySelector('#user-btn').onclick = () => {
+    profile.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
 
-document.querySelector('#user-button').addEventListener("click",() =>{
-    profile.classList.toggle('active')
-})
+// SEARCH BUTTON
+let searchForm = document.querySelector('.header .flex .search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    profile.classList.remove('active')
+}
 
-window.onscroll = () =>{
-        profile.classList.remove('active')
+let sidebar = document.querySelector('.side-bar');
+document.querySelector('#menu-btn').onclick = () => {
+ sidebar.classList.toggle('active');
+ body.classList.toggle('active')
+}
+
+// SCROLL EVENT
+window.onscroll = () => {
+    profile.classList.remove('active');
+    searchForm.classList.remove('active');
+    sidebar.classList.remove('active');
 }
