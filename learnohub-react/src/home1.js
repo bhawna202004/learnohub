5af9524a2381a1576b227066fb9522dbecac160d
= <bgarg6675@gmail.com>
2026-04-14 22:15:47 +0530

    done

---
+++ learnohub-react/src/home1.js
@@ -0,0 +1,10 @@
+function home1() {
+  return (
+    <div>
+      <h1>Learn Anytime, Anywhere 📚</h1>
+      <p>Welcome to LearnoHub - Your learning platform</p>
+    </div>
+  );
+}
+
+export default home1;
